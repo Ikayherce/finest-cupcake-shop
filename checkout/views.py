712a6c52d560840982e680c9aa8a34cf50db2a4e
Ika Yébenes Herce <ikayherce@gmail.com>
2024-05-25 09:28:00 +0000

    add stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51PKGHHDUgyvSj23RYm2LKUyTlBao2HTQh8fjslVw8ulv2Mi1buNPdlP3KftDWA3CLqECydSArRwmrDqollWTC2UI00uNuMqyoX',
+        'client_secret': 'test client secret', 
     }
 
     return render(request, template, context)
