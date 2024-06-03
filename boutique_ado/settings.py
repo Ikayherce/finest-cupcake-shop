63d853892fc4027457a640f0986aa1b6d8ea8970
Ika Yébenes Herce <ikayherce@gmail.com>
2024-06-03 14:32:12 +0000

    add deployment s3 changes

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -185,7 +185,7 @@ if 'USE_AWS' in os.environ:
     }
 
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'finest-cupcake-shop'
+    AWS_STORAGE_BUCKET_NAME = 'the-finest-cupcake-shop'
     AWS_S3_REGION_NAME = 'us-east-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
